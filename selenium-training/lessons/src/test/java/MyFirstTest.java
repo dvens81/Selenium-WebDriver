cef973e5a9a7ad445dd35b34fb8a5637f764a8d7
dvens81 <dvens81@mail.ru>
2022-04-05 18:33:19 +0400

    task3

--- selenium-training/lessons/src/test/java/MyFirstTest.java
+++ selenium-training/lessons/src/test/java/MyFirstTest.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class MyFirstTest {
@@ -16,6 +18,7 @@ public class MyFirstTest {
     @Before
     public void  start() {
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         //wait = new WebDriverWait(driver,10);
     }
 
