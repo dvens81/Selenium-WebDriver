5c13e548ace1b65e76448f9c5b868e0f7400c542
dvens81 <dvens81@mail.ru>
2022-04-09 19:01:33 +0400

    Поиск элемента по css селектору. Получение значения свойства атрибута

--- selenium-training/lessons/src/test/java/lesson5_getAttribute.java
+++ selenium-training/lessons/src/test/java/lesson5_getAttribute.java
@@ -22,9 +22,19 @@ public class lesson5_getAttribute {
 
     @Test
     public void myFirstTest() {
+
         driver.get("http://localhost/litecart/en/");
+
         String a = driver.findElement(By.cssSelector("#breadcrumbs a")).getAttribute("href");
         System.out.println("Значение свойства href = " + a);
+
+        String a2 = driver.findElement(By.cssSelector("div.content h3")).getCssValue("color");
+        System.out.println("Значение свойства color = " + a2);
+
+        //Селектор невидимый. Находится во фрейме
+//        String b = driver.findElement(By.cssSelector("[name=currency_code] [value=USD]")).getAttribute("selected");
+//        System.out.println("Значение свойства selected = " + b);
+
         //wait.until(titleIs("webdriver - Поиск в Google"));
     }
 
