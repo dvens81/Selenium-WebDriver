bd233a0661ccabf1b448a9b586e0997c3a6926b3
dvens81 <dvens81@mail.ru>
2022-04-23 15:37:40 +0400

    task8

---
+++ selenium-training/lessons/src/test/java/task8.java
@@ -0,0 +1,60 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class task8 {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void myFirstTest() {
+
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
+
+        ArrayList<String> countries_List = new ArrayList<>();
+
+        List<WebElement> countries = driver.findElements(By.cssSelector(".row td:nth-child(5) a"));
+        for (int i = 0; i < countries.size(); i++) {
+            String attr_Countries = countries.get(i).getAttribute("textContent");
+            countries_List.add(i, attr_Countries);
+        }
+        System.out.println(countries_List);
+
+        ArrayList<String> countries_sort_List = new ArrayList<>();
+        for (int i = 0; i < countries_List.size(); i++) {
+            countries_sort_List.add(i, countries_List.get(i));
+        }
+
+        Collections.sort(countries_sort_List);
+        System.out.println(countries_sort_List);
+        Assert.assertEquals(countries_List, countries_sort_List);
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
