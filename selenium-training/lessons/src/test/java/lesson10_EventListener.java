ff8fc9ed09db05a10bceb4ce766a1b83908b06d3
dvens81 <dvens81@mail.ru>
2022-04-28 12:09:05 +0400

    lesson10_EventListener
    Протоколирование

---
+++ selenium-training/lessons/src/test/java/lesson10_EventListener.java
@@ -0,0 +1,55 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class lesson10_EventListener {
+    private EventFiringWebDriver driver;
+    private WebDriverWait wait;
+
+    public static class MyListener extends AbstractWebDriverEventListener{
+        @Override
+        public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println(by);
+        }
+
+        @Override
+        public void afterFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println(by + " found");
+        }
+
+        @Override
+        public void onException(Throwable throwable, WebDriver driver) {
+            System.out.println(throwable);
+        }
+    }
+
+    @Before
+    public void start() {
+        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver.register(new MyListener());
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("http://www.google.com/");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.name("btnK")).click();
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
