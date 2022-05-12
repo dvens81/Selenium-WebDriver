f2a59b8e4080b7087b2d435d63b75383261b41ce
dvens81 <dvens81@mail.ru>
2022-05-12 16:48:24 +0400

    task14
    Открытие нового окна. Переключения между окнами. Метод ожидания появления нового окна с возвращением его идентификатора

---
+++ selenium-training/lessons/src/test/java/task14.java
@@ -0,0 +1,68 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class task14 {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    }
+
+    //Ожидание нового окна. Возвращает идентификатор нового окна
+    public ExpectedCondition<String> anyWindowOtherThan(Set<String> oldWindows) {
+        return new ExpectedCondition<String>() {
+            public String apply(WebDriver driver) {
+                Set<String> handles = driver.getWindowHandles();
+                handles.removeAll(oldWindows);
+                return handles.size() > 0 ? handles.iterator().next() : null;
+            }
+        };
+    }
+
+    @Test
+    public void Test() {
+
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
+        driver.get("http://localhost/litecart/admin/?app=countries&doc=edit_country&country_code=AF");
+
+        List<WebElement> links = driver.findElements(By.cssSelector(".fa-external-link"));
+        for (int i = 0; i < links.size(); i++) {
+            String originalWindow = driver.getWindowHandle();
+            Set<String> existingWindows = driver.getWindowHandles();
+            links.get(i).click();
+            String newWindow = wait.until(anyWindowOtherThan(existingWindows));
+            driver.switchTo().window(newWindow);
+            driver.close();
+            driver.switchTo().window(originalWindow);
+        }
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
